2c2006d9570c155659e12c08c92e6e8a6a85e4ed
Myr7You <myr7you@gmail.com>
2020-04-17 23:40:21 +0800

    deploy

---
+++ precache-manifest.2e835e525812ff371b28f28b71972b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2acd78b770f86a1f135",
+    "url": "/real_world_vue/css/app.71c5d0f9.css"
+  },
+  {
+    "revision": "855628d2ba3687e318276ba9236922eb",
+    "url": "/real_world_vue/feather-sprite.svg"
+  },
+  {
+    "revision": "0f6af201108f72526d1ebb0eac2ef4a4",
+    "url": "/real_world_vue/index.html"
+  },
+  {
+    "revision": "a2acd78b770f86a1f135",
+    "url": "/real_world_vue/js/app.d62e2cbf.js"
+  },
+  {
+    "revision": "8ae0ba664b04ec6f6661",
+    "url": "/real_world_vue/js/chunk-vendors.d95d1611.js"
+  },
+  {
+    "revision": "207ac4040e86bb707e3f83c31e86551a",
+    "url": "/real_world_vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/real_world_vue/robots.txt"
+  }
+]);
